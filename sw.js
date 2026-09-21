1aaa3ea3caf767d8d4e5f288e28522732abd04cc
puren21 <147798791+puren21@users.noreply.github.com>
2026-09-21 17:03:31 +0900

    Refresh service worker cache for optimized build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='flight-route-v9';
+const CACHE='flight-route-v10';
 const APP_SHELL=['./flight-route.geojson','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
@@ -30,7 +30,7 @@ self.addEventListener('fetch',event=>{
     url.pathname.endsWith('/flight-route/');
 
   if(isNavigation){
-    const freshUrl=new URL('./index.html?build=20260921-1243',self.registration.scope).href;
+    const freshUrl=new URL('./index.html?build=20260921-optimized',self.registration.scope).href;
     event.respondWith(
       fetch(freshUrl,{
         cache:'no-store',
